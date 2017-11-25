42bf954d8856ea96cedf4142b80777c8b80775c2
Simon Peter Orozco <Simon@Simons-MBP-256.hsd1.md.comcast.net>
2017-11-24 20:57:39 -0500

    Added Velocity and direction to generate_new_variables outputs and created the function hist_dependence to create the covariates matrix when using spike history

--- generate_new_variables.m
+++ generate_new_variables.m
@@ -3,7 +3,7 @@
 % This function returns cell arrays of Interspike intervals (ISIs), X
 % Locations at spike times, and Y Locations at spike times.
 
-function [ISI,XLocAtSpikes,YLocAtSpikes] = generate_new_variables(x,y,spikes)
+function [ISI,XLocAtSpikes,YLocAtSpikes,Vx,Vy,dir] = generate_new_variables(x,y,spikes)
 
 n = size(spikes,2);
 
@@ -15,6 +15,9 @@ for i = 1:n
     ISI{i} = diff(find(spikes(:,i)));
     XLocAtSpikes{i} = x(spikes(:,i));
     YLocAtSpikes{i} = y(spikes(:,i));
+    Vx = diff(x)*60;
+    Vy = diff(y)*60;
+    dir = atan2((y(2:end)-y(1:end-1)),x(2:end)-x(1:end-1));
 end
 
 end
---
+++ hist_dependence.m
@@ -0,0 +1,36 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% hist_depenence.m
+%
+% This function creates the matrix of covariates to be used in glmfit when
+% spike history is included in the model.
+%
+% Inputs: num_backsteps - how far back in time to include as covariates
+%         spikes - vector of spikes for this neuron
+%         varargin - vectors of the non-spike history covariates to be 
+%         included
+%
+% Output: covariate_matrix - the matrix of covariates. Use this with glmfit
+%
+% Function by: Simon Orozco
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function covariate_matrix = hist_dependence(num_backsteps,spikes,varargin)
+
+n_cov = length(varargin); %Number of non-spike covariates
+tot_cov = n_cov + num_backsteps; %Total number of covariates
+
+%Preallocate for speed
+covariate_matrix = NaN(length(varargin{1}),tot_cov);
+
+% Add non-spike covariates to matrix
+for i = 1:n_cov
+    covariate_matrix(:,i) = varargin{i}(num_backsteps+1:end);
+end
+
+% Add spike history dependent covariates to matrix
+for n = 1:num_backsteps
+    covariate_matrix(:,i+n) = spikes((num_backsteps-(n-1)):end-(n-1));
+end
+
+end
+
