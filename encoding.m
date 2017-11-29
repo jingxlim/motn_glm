5c4c0c1ea8a6a0569a540af4a5ca29d8a5f30621
Lim Jing Xuan <jingx.lim@outlook.com>
2017-11-28 21:12:08 -0500

    test fitting downsampled data

--- encoding.m
+++ encoding.m
@@ -11,20 +11,31 @@ close all; % close previous plots
 %% load data & generate variables
 
 load('train.mat')
-[ISI,XLocAtSpikes,YLocAtSpikes,Vx,Vy,dir] = generate_new_variables(xN,yN,spikes_binned);
-% generate directions?
+[ISI,XLocAtSpikes,YLocAtSpikes] =...
+    gen_spike_stat(xN,yN,spikes_binned);
 
-ISI_threshold = 550;
+
 %%%% notes %%%%
 % more variables could be useful but also means more to present
 
 %% plotting raw data
-% plot_spiking_times
+% plot spike times as a function of location
 plot_spiking_positions(xN,yN,XLocAtSpikes,YLocAtSpikes,'subplot');
+
+% plot ISIs
+ISI_threshold = 550;
 ISIs = plot_ISIs(spikes_binned,ISI_threshold,2);
 
 % plot_spiking_velocities maybe?
 % plot_spiking_directions maybe?
+
+%% downsample data
+ds_rate = 50;  % Hz
+[xN_ds,yN_ds,spikes_binned_ds] = downsample(xN,yN,spikes_binned,ds_rate);
+
+%% generate new covariates
+[Vx,Vy,dir] = generate_new_variables(xN,yN,spikes_binned,1000);  % raw data
+[Vx_ds,Vy_ds,dir_ds] = generate_new_variables(xN_ds,yN_ds,spikes_binned_ds,ds_rate);
 
 %% classifying cells
 % This needs to be automated, and it turns out that is a hard problem (at
@@ -62,46 +73,59 @@ for i=1:size(spikes_binned,2)  % iterate through all the neurons
     lambdaEst{2} = gen_lambda(b2,covar_m2);
     spikess{2} = spikes;
     
-    % Model 3: linear + quadratic + integrate + history dependence
-    hist = 1:120;
-    [spikes_m3,covar_m3] = hist_dep(hist,spikes,xN,yN,xN.^2,yN.^2,xN.*yN);
-    [b3,dev3,stats3] = glmfit(covar_m3,spikes_m3,'poisson');
+    spikes_ds = spikes_binned_ds(:,i);
+    % Model 3: linear + quadratic (downsampled)
+    covar_m3 = [xN_ds yN_ds xN_ds.^2 yN_ds.^2];
+    [b3,dev3,stats3] = glmfit(covar_m3,spikes_ds,'poisson');
     lambdaEst{3} = gen_lambda(b3,covar_m3);
-    spikess{3} = spikes_m3;
+    spikess{3} = spikes_ds;
     
-    % EVALUATE MODELS
-    figure(); clf; hold on;
-    set(gcf,'units','points','position',[100,100,1000,400])
-
-    subplot(1,2,1); hold on;
-    [x_new,y_new] = meshgrid(-1:.1:1);
-    y_new = flipud(y_new);
-    x_new = fliplr(x_new);
-
-    % compute lambda for each point on this grid using the GLM model
-    lambda3 = lambdaEst{3};
-    lambda3(x_new.^2 + y_new.^2 > 1)=nan;
-
-    % plot lambda as a function position over this grid
-    h_mesh = mesh(x_new,y_new,lambda3,'AlphaData',0);
-    get(h_mesh,'AlphaData');
-    set(h_mesh,'AlphaData',0);
-    hold on;
-    plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
-    xlabel('x position [m]'); ylabel('y position [m]');
+    % Model 4: linear + quadratic + integrate (downsampled)
+    covar_m4 = [xN_ds yN_ds xN_ds.^2 yN_ds.^2 xN_ds.*yN_ds];
+    [b4,dev4,stats4] = glmfit(covar_m4,spikes_ds,'poisson');
+    lambdaEst{4} = gen_lambda(b4,covar_m4);
+    spikess{4} = spikes_ds;    
     
-    % plot betas
-    subplot(1,2,2); hold on;
-    for n=1:numel(b3)
-        plot(n,b3(n),'*','DisplayName',num2str(stats3.p(n)));
-    end
-    legend('show','Location','bestoutside')
-    errorbar(b3,2*stats3.se);
-    xticks(1:length(b3));
-    xlim([0 length(b3)+1]);
-    xlabel('\beta number'); ylabel('\beta value');
-    saveas(gcf, ['betas-neuron_' num2str(i) '.png'])
-    
+%     % Model 3: linear + quadratic + integrate + history dependence
+%     hist = 1:120;
+%     [spikes_m3,covar_m3] = hist_dep(hist,spikes,xN,yN,xN.^2,yN.^2,xN.*yN);
+%     [b3,dev3,stats3] = glmfit(covar_m3,spikes_m3,'poisson');
+%     lambdaEst{3} = gen_lambda(b3,covar_m3);
+%     spikess{3} = spikes_m3;
+%     
+%     % EVALUATE MODELS
+%     figure(); clf; hold on;
+%     set(gcf,'units','points','position',[100,100,1000,400])
+% 
+%     subplot(1,2,1); hold on;
+%     [x_new,y_new] = meshgrid(-1:.1:1);
+%     y_new = flipud(y_new);
+%     x_new = fliplr(x_new);
+% 
+%     % compute lambda for each point on this grid using the GLM model
+%     lambda3 = lambdaEst{3};
+%     lambda3(x_new.^2 + y_new.^2 > 1)=nan;
+% 
+%     % plot lambda as a function position over this grid
+%     h_mesh = mesh(x_new,y_new,lambda3,'AlphaData',0);
+%     get(h_mesh,'AlphaData');
+%     set(h_mesh,'AlphaData',0);
+%     hold on;
+%     plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
+%     xlabel('x position [m]'); ylabel('y position [m]');
+%     
+%     % plot betas
+%     subplot(1,2,2); hold on;
+%     for n=1:numel(b3)
+%         plot(n,b3(n),'*','DisplayName',num2str(stats3.p(n)));
+%     end
+%     legend('show','Location','bestoutside')
+%     errorbar(b3,2*stats3.se);
+%     xticks(1:length(b3));
+%     xlim([0 length(b3)+1]);
+%     xlabel('\beta number'); ylabel('\beta value');
+%     saveas(gcf, ['betas-neuron_' num2str(i) '.png'])
+%     
     % plot KS plots for all three models
     plot_ks(spikess,lambdaEst);
     cur_title = get(gca, 'Title');
--- generate_new_variables.m
+++ generate_new_variables.m
@@ -3,18 +3,11 @@
 % This function returns cell arrays of Interspike intervals (ISIs), X
 % Locations at spike times, and Y Locations at spike times.
 
-function [ISI,XLocAtSpikes,YLocAtSpikes,Vx,Vy,dir] = generate_new_variables(x,y,spikes,pos_sampling_rate)
+function [Vx,Vy,dir] = generate_new_variables(x,y,spikes,pos_sampling_rate)
 
 n = size(spikes,2);
 
-ISI = cell(n,1);
-XLocAtSpikes = cell(n,1);
-YLocAtSpikes = cell(n,1);
-
 for i = 1:n
-    ISI{i} = diff(find(spikes(:,i)));
-    XLocAtSpikes{i} = x(spikes(:,i));
-    YLocAtSpikes{i} = y(spikes(:,i));
     Vx = diff(x)*pos_sampling_rate;
     Vy = diff(y)*pos_sampling_rate;
     dir = atan2((y(2:end)-y(1:end-1)),x(2:end)-x(1:end-1));
